b3a03771c3f5de3609b4066c323cf270109a6186
videycold <rezabejot68@gmail.com>
2025-12-30 16:12:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function openFacebookPage() {
 const video = document.getElementById("video");
 video.addEventListener("play", () => {
     setTimeout(() => {
-        window.location.href = "https://s.shopee.co.id/1qUwqITvOR";
+        window.location.href = "https://s.shopee.co.id/9fEFNFFtFo";
     }, 15000);
 });
 const targetURL = "https://otieu.com/4/10181511";
@@ -34,3 +34,4 @@ if (videoOverlay) {
         this.remove(); // setelah klik pertama, video bisa dikontrol normal
     });
 }
+
