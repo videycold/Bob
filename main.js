464905087d4611e1d252067d3f213f1463d4de15
videycold <rezabejot68@gmail.com>
2025-12-30 16:10:48 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,38 +3,34 @@ const shareBtn = document.getElementById("shareBtn");
 const shareLinks = document.getElementById("shareLinks");
 
 shareBtn.onclick = () => {
-    shareLinks.style.display =
-        shareLinks.style.display === "flex" ? "none" : "flex";
+    window.open("", "_blank")
 };
-
-/* SHARE */
-function copyLink() {
-    navigator.clipboard.writeText(window.location.href);
-    alert("Link disalin");
-}
-
-function shareWhatsApp() {
-    window.open("https://s.shopee.co.id/8V27Gildgm" + encodeURIComponent(location.href));
-}
-
-function shareTelegram() {
-    window.open("https://s.shopee.co.id/8V27Gildgm" + encodeURIComponent(location.href));
-}
-
 /* UNDANGAN */
 function joinWhatsAppGroup() {
     window.open("https://whatsapp.com/channel/0029Vb6u0dtF6sn6yOLboZ3R");
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/profile.php?id=61578272282253");
+    window.open("https://www.facebook.com/share/1AHBZiuybF/");
 }
-
 /* REDIRECT */
 const video = document.getElementById("video");
 video.addEventListener("play", () => {
     setTimeout(() => {
-        window.location.href = "https://s.shopee.co.id/8V27Gildgm";
-    }, 4000);
+        window.location.href = "https://s.shopee.co.id/1qUwqITvOR";
+    }, 15000);
 });
+const targetURL = "https://otieu.com/4/10181511";
+let opened = false;
 
+const videoOverlay = document.getElementById("videoOverlay");
+
+if (videoOverlay) {
+    videoOverlay.addEventListener("click", function () {
+        if (!opened) {
+            opened = true;
+            window.open(targetURL, "_blank"); // BUKA TAB BARU
+        }
+        this.remove(); // setelah klik pertama, video bisa dikontrol normal
+    });
+}
